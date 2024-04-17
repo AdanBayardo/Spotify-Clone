8187771c64b583a757297854ae6f887912ce7d87
unknown <adancastilloduarte@gmail.com>
2024-04-17 10:37:11 +0200

    Create Player component                 -Create CardPlayButton component Create Greeting component

---
+++ src/components/CardPlayButton.jsx
@@ -0,0 +1,17 @@
+
+import { Play, Pause } from "./Player"
+import { usePlayerStore } from "@/store/playerStore"
+export function CardPlayButton (){
+
+    const {currentSong, isPlaying, setIsPlaying, setCurrentSong} = usePlayerStore(state => state)
+    const handleClick = () => {
+        setIsPlaying(!isPlaying)
+
+    }
+
+    return (    
+        <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4">
+            {isPlaying ? <Pause/> : <Play/>}
+        </button>
+    )
+}
